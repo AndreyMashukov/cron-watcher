74b2ec946ab71f09bad53a302ad4a973b5efd835
AMashukov <AMashukov@itsupportme.com>
2018-05-18 13:11:25 +0300

    Created cron watcher

---
+++ src/CronWatcher.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AM\CronWatcher;
+
+use Symfony\Component\Process\Exception\LogicException;
+
+class CronWatcher
+{
+    /**
+     * Check process signal and kill current process if script already run.
+     *
+     * @see {http://man7.org/linux/man-pages/man7/signal.7.html}
+     */
+    public static function run()
+    {
+        $tmpPath = \sys_get_temp_dir();
+
+        if (false === isset($argv)) {
+            $argv = [];
+        } //end if
+
+        $name = \md5(\implode('_', $argv));
+        $path = $tmpPath . '/' . $name;
+
+        if (true === \file_exists($path)) {
+            $pid = \file_get_contents($path);
+
+            if (\posix_kill($pid, 0)) {
+                throw new LogicException('Script already ran!');
+            } //end if
+        } //end if
+
+        $pid = \posix_getpid();
+        \file_put_contents($path, $pid);
+    }
+}
---
+++ tests/CronWatcherTest.php
@@ -0,0 +1,85 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Process;
+
+class CronWatcherTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    private $scriptPath;
+
+    public function setUp()
+    {
+        $script = "<?php\n"
+            . "require __DIR__ . '/../vendor/autoload.php';\n\n"
+            . "\\AM\\CronWatcher\\CronWatcher::run();\n\n"
+            . "echo 'script started';\n"
+            . '\\sleep(50);';
+
+        $this->scriptPath = __DIR__ . '/test_script.php';
+
+        \file_put_contents($this->scriptPath, $script);
+    }
+
+    public function tearDown()
+    {
+        \unlink($this->scriptPath);
+    }
+
+    /**
+     * Should allow to run script by CLI only one time.
+     *
+     * @group integration
+     */
+    public function testShouldAllowToRunScriptByCliOnlyOneTime()
+    {
+        $process = new Process('php ' . $this->scriptPath);
+        $process->start();
+
+        \sleep(5);
+
+        $this->assertEquals('script started', $process->getOutput());
+
+        $secondProcess = new Process('php ' . $this->scriptPath);
+        $secondProcess->start();
+
+        \sleep(5);
+
+        $this->assertNotEquals('script started', $secondProcess->getOutput());
+
+        $process->stop();
+        $secondProcess->stop();
+    }
+
+    /**
+     * Should allow to run script by CLI only one time.
+     *
+     * @group unit
+     */
+    public function testUnitShouldAllowToRunScriptByCliOnlyOneTime()
+    {
+        $tmpPath = \sys_get_temp_dir();
+        $name    = 'd41d8cd98f00b204e9800998ecf8427e';
+
+        $path = $tmpPath . '/' . $name;
+
+        if (true === \file_exists($path)) {
+            \unlink($path);
+        }
+
+        $this->assertFileNotExists($path);
+
+        \AM\CronWatcher\CronWatcher::run();
+
+        $this->assertFileExists($path);
+
+        $pid = \file_get_contents($path);
+        $this->assertRegExp('/\d+/ui', $pid);
+
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('Script already ran!');
+        \AM\CronWatcher\CronWatcher::run();
+    }
+}
